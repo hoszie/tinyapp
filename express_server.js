f79a1e01ea4ecbf72b7526736d6793883472091d
Nick <nhoszko@gmail.com>
2019-10-29 00:03:47 +0000

    Rendering urls to browser using template

--- express_server.js
+++ express_server.js
@@ -21,6 +21,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
+// sending urls inside an object so that we can use the key (urls) to access the data within our template  ///
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
